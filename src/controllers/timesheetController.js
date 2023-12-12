bc10063ba09a8184fce7be005b161599bb56d119
Padam Sethia <padamsethia5@gmail.com>
2023-12-12 12:53:11 +0530

    Update createTimesheeet

--- src/controllers/timesheetController.js
+++ src/controllers/timesheetController.js
@@ -23,77 +23,67 @@ const createTimesheets = async (req, res) => {
         if (!existingEmployee) {
           return { error: `Employee with ID ${EmployeeID} not found` };
         }
-        const date = new Date(entryDate);
-        if (existingTimesheet) {
-          // Update the existing timesheet entry
-          const updatedTimesheet = await timesheetModel.updateTimesheet(existingTimesheet.id, {
-            Status,
-            HoursWorked,
-            Description,
-          });
-          return updatedTimesheet;
-        } else {
-          const timesheetData = {
-            EmployeeID,
-            ProjectID,
-            Date: date,
-            Status,
-            HoursWorked,
-            Description,
-          };
-          const timesheet = await timesheetModel.createTimesheet(timesheetData);
-          return timesheet;
-        }
-      })
-    );
-    res.json(results);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-const getAllTimesheetdata = async (req, res) => {
-  try {
-    const { EmployeeID, startDate, endDate } = req.body;
-    const existingEmployee = await prisma.employee.findMany({
-      where: {
-        EmployeeID: EmployeeID,
-      },
-    });
-    if (!existingEmployee) {
-      return res.status(404).json({ error: 'Employee not found' });
-    }
-    const dummyTimesheet = {
-      EmployeeID: EmployeeID,
-      ProjectID: 1,
-      Date: randomDate(new Date(startDate), new Date(endDate), 0, 0),
-      Status: 'submitted',
-      HoursWorked: Math.floor(Math.random() * 8) + 1,
-      Description: 'Random Description',
-    };
-    const createdTimesheet = await prisma.timesheet.create({
-      data: dummyTimesheet,
-    });
+        const date = new Date(entryDate).setHours(0, 0, 0, 0);
 
-    res.json(dummyTimesheet);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
 
-  function randomDate(start, end, startHour, endHour) {
-    var date = new Date(+start + Math.random() * (end - start));
-    var hour = startHour + Math.random() * (endHour - startHour) | 0;
-    date.setHours(hour);
-    return date;
-  }
-}
+        const createTimesheets = async (req, res) => {
+          try {
+            const timesheetEntries = req.body.timesheets;
+            if (!timesheetEntries || !Array.isArray(timesheetEntries)) {
+              return res.status(400).json({ error: 'Invalid timesheetEntries in the request body' });
+            }
+            const results = await Promise.all(
+              timesheetEntries.map(async (entry) => {
+                const { EmployeeID, ProjectID, entryDate, Status, Description, HoursWorked, EntryType } = entry;
+                const existingEmployee = await prisma.employee.findUnique({
+                  where: {
+                    EmployeeID: EmployeeID,
+                  },
+                });
+                if (!existingEmployee) {
+                  return { error: `Employee with ID ${EmployeeID} not found` };
+                }
+                const date = new Date(entryDate).setHours(0, 0, 0, 0);
 
-const getTimesheetList = async (req, res) => {
-  try {
-    const timesheets = await timesheetModel.getTimesheets();
-    res.json(timesheets);
-  } catch (error) {
+                const existingTimesheet = await prisma.timesheet.findFirst({
+                  where: {
+                    EmployeeID: EmployeeID,
+                    ProjectID: ProjectID,
+                    Date: date,
+                  },
+                });
+
+                if (existingTimesheet) {
+                  // Update the existing timesheet entry
+                  const updatedTimesheet = await timesheetModel.updateTimesheet(existingTimesheet.id, {
+                    Status,
+                    HoursWorked,
+                    Description,
+                  });
+                  return updatedTimesheet;
+                } else {
+                  const timesheetData = {
+                    EmployeeID,
+                    ProjectID,
+                    Date: date,
+                    Status,
+                    HoursWorked,
+                    Description,
+                  };
+                  const timesheet = await timesheetModel.createTimesheet(timesheetData);
+                  return timesheet;
+                }
+              })
+            );
+            res.json(results);
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+          }
+        };
+      }))
+  }
+  catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -267,8 +257,8 @@ const getEmployeesUnderManagerOnSameProject = async (req, res) => {
               where: {
                 ProjectID: projectId,
                 Date: {
-                  gte: new Date(startDate),
-                  lte: new Date(endDate),
+                  gte: new Date(startDate).setHours(0, 0, 0, 0),
+                  lte: new Date(endDate).setHours(0, 0, 0, 0),
                 },
               },
             },
